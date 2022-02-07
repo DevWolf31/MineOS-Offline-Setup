3af2295034d43ba85be21ed371cf99e6e7445949
Marcos-MD <githubmmd09@gmail.com>
2022-02-06 22:30:20 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 do
   local addr, invoke = computer.getBootAddress(), component.invoke
+  -- opencomputers loadfile() inplementation
   local function loadfile(file)
+    local handle = assert(invoke(addr, "open", file))
+    local buffer = ""
+    repeat
+      local data = invoke(addr, "read", handle, math.huge)
+      buffer = buffer .. (data or "")
+    until not data
+    invoke(addr, "close", handle)
+    return load(buffer, "=" .. file, "bt", _G)
   end
 end
